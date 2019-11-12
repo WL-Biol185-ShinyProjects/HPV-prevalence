0b68c49ea528efd19901f7c575d435a3aff24ead
Whitley Drinkard <drinkardw20@mail.wlu.edu>
2019-11-12 12:56:25 -0500

    Code for bar graph of HPV realted cancer types as realted to age. Does not work.

--- server.r
+++ server.r
@@ -1,1 +1,15 @@
-#HPV_Prevalence is Tidy Data!!!
+library(shiny)
+library(ggplot2)
+library(tidyverse)
+
+
+
+function(input, output){
+  
+  output$HPV_relcancers_prevalencebyage <- renderPlot({
+    
+    HPV_cancers %>%
+      ggplot(aes_string("country", "age_range", fill = "age range")) + geom_col(alpha = 0.2)
+  })
+  
+}
--- ui.r
+++ ui.r
@@ -1,1 +1,37 @@
-View(HPV_Prevalence)
+library(shiny)
+library(ggplot2)
+library(tidyverse)
+
+
+fluidPage(
+  
+  title = "HPV Related Cancers and Prevalence in Different Countries",
+  titlePanel("HPV Realted Cancers"),
+  sidebarLayout(
+    sidebarPanel(
+      selectInput(
+        inputId = "country",
+        label = "Country",
+        choices = list(unique(HPV_cancers$country),
+          
+        ),
+        multiple = TRUE,
+        selected = unique(HPV_cancers$country)
+      ),
+      selectizeInput(
+        
+        
+        inputId = "age_range",
+        label = "Age Range",
+        choices = unique(HPV_cancers$age_range),
+        multiple = TRUE,
+        selected = unique(HPV_cancers$age_range)
+        
+      )
+    ),
+    
+    mainPanel(
+      plotOutput("HPV_relcancers_prevalencebyage")
+    )
+  )
+)
