c6472abeab65d8f31f783e3d53c53ba2cbfb9a4e
Mikki Whittington <whittingtono20@mail.wlu.edu>
2019-11-12 13:16:25 -0500

    Very very beginning stages of bar graph

--- server.r
+++ server.r
@@ -1,1 +1,14 @@
-#HPV_Prevalence is Tidy Data!!!
+library(shiny)
+library(ggplot2)
+library(tidyverse)
+
+function(input, output) {
+  
+  output$country.density <- renderPlot({
+    HPV_Prevalence %>%
+      filter(Country %in% input$prevalence.include.countries) %>%
+      ggplot(aes_string("HPV_Prevalence", fill = input$prevalence.group.by)) +
+      geom_density(alpha = 0.2)
+  })
+  
+}
--- ui.r
+++ ui.r
@@ -1,1 +1,33 @@
-View(HPV.type.prevalence)
+View(HPV_Prevalence)
+
+library(shiny)
+library(ggplot2)
+
+fluidPage(
+  
+  title = "HPV Interactive",
+  titlePanel("Prevalence of HPV Types"),
+  sidebarLayout(
+    sidebarPanel(
+      selectInput(
+        inputId  = "prevalence.group.by",
+        label    = "Input",
+        choices  = list(Country = "Country", 
+                        Type    = "HPV_Type"),
+        selected = "Country"
+        
+      ), 
+      selectizeInput(
+        inputId  = "prevalence.include.countries",
+        label    = "Include Countries",
+        choices  = unique(HPV_Prevalence$Country), 
+        multiple = FALSE,
+        selected = unique(HPV_Prevalence$Country)
+      )
+    ),
+    
+    mainPanel(
+      plotOutput("country.density")
+    )
+  )
+)
