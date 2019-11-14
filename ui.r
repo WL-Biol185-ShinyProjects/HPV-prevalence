7e80c4f699469aa8c9fa2eb19f8fff885221e1d3
Mikki Whittington <whittingtono20@mail.wlu.edu>
2019-11-14 13:15:10 -0500

    Figuring Out Missing Argument

--- ui.r
+++ ui.r
@@ -22,59 +22,58 @@ dashboardPage(
     )
   ),
   dashboardBody(
-    tabItems(
-      tabItem(tabname = "HPV_types", 
+     tabItems(
+      tabItem(tabname = "HPV_types",
               sidebarLayout(
                 sidebarPanel(
                   selectInput(
-                    inputId  = "prevalence.group.by",
+                    inputId  = "Prevalence_group_by",
                     label    = "Input",
-                    choices  = list(Country = "Country", 
+                    choices  = list(Country = "Country",
                                     Type    = "HPV_Type"),
                     selected = "Country"
-                    
-                  ), 
+
+                  ),
                   selectizeInput(
-                    inputId  = "prevalence.include.countries",
+                    inputId  = "Prevalence_include_countries",
                     label    = "Include Countries",
-                    choices  = unique(HPV_Prevalence$Country), 
+                    choices  = unique(HPV_Prevalence$Country),
                     multiple = FALSE,
                     selected = unique(HPV_Prevalence$Country)
                   )
+                ),
+
+                mainPanel(
+                  plotOutput("Country_density")
                 )
               ),
-              
-              mainPanel(
-                plotOutput("Country_density")
-              )
-            ), 
+            ),
       
-      tabItem(tabName = "Related_cancers", 
-              sidebarPanel(
-                selectInput(
-                  inputId = "country",
-                  label = "Country",
-                  choices = list(unique(HPV_cancers$country),
-                                 
-                  ),
-                  multiple = TRUE,
-                  selected = unique(HPV_cancers$country)
-                ),
-                selectizeInput(
-                  
-                  
-                  inputId = "age_range",
-                  label = "Age Range",
-                  choices = unique(HPV_cancers$age_range),
-                  multiple = TRUE,
-                  selected = unique(HPV_cancers$age_range)
-                ), 
-                
-                mainPanel(
-                  plotOutput("HPV_relcancers_prevalencebyage")
-                )
-              )
-            )
-          )
+      # tabItem(tabName = "Related_cancers",
+      #         sidebarLayout(
+      #         sidebarPanel(
+      #           selectInput(
+      #             inputId  = "country",
+      #             label    = "Country",
+      #             choices  = list(unique(HPV_cancers$country)),
+      #             multiple = TRUE,
+      #             selected = unique(HPV_cancers$country)
+      #           ),
+      # 
+      #           selectizeInput(
+      #             inputId  = "age_range",
+      #             label    = "Age Range",
+      #             choices  = unique(HPV_cancers$age_range),
+      #             multiple = TRUE,
+      #             selected = unique(HPV_cancers$age_range)
+      #           )
+      #         ),
+      # 
+      #         mainPanel(
+      #           plotOutput("HPV_relcancers_prevalencebyage")
+      #         )
+      #       )
+      #     )
         )
       )
+    )
