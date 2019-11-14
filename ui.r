49ca6ebd131509044817834e041178c5cb045c1e
Mikki Whittington <whittingtono20@mail.wlu.edu>
2019-11-14 12:39:51 -0500

    Matching up Styles

--- ui.r
+++ ui.r
@@ -1,66 +1,80 @@
 View(HPV_Prevalence)
+View(HPV_cancers)
 
 library(shiny)
+library(shinydashboard)
 library(ggplot2)
 library(tidyverse)
 
-fluidPage(
-  
-  title = "HPV Interactive",
-  titlePanel("Prevalence of HPV Types"),
-  sidebarLayout(
-    sidebarPanel(
-      selectInput(
-        inputId  = "prevalence.group.by",
-        label    = "Input",
-        choices  = list(Country = "Country", 
-                        Type    = "HPV_Type"),
-        selected = "Country"
+dashboardPage(
+  dashboardHeader(title = "HPV and Related Cancers"),
+  dashboardSidebar(
+    sidebarMenu(
+      menuItem("Dashboard", 
+               tabName = "dashboard", 
+               icon    = icon("dashboard")), 
+      menuItem("HPV Types", 
+               icon    = icon("angle-right"), 
+               tabName = "HPV_types"), 
+      menuItem("Related Cancers", 
+               icon    = icon("angle-right"), 
+               tabName = "Related_cancers")
+    )
+  ),
+  dashboardBody(
+    tabItems(
+      tabItem(tabname = "HPV_types", 
+              sidebarLayout(
+                sidebarPanel(
+                  selectInput(
+                    inputId  = "prevalence.group.by",
+                    label    = "Input",
+                    choices  = list(Country = "Country", 
+                                    Type    = "HPV_Type"),
+                    selected = "Country"
+                    
+                  ), 
+                  selectizeInput(
+                    inputId  = "prevalence.include.countries",
+                    label    = "Include Countries",
+                    choices  = unique(HPV_Prevalence$Country), 
+                    multiple = FALSE,
+                    selected = unique(HPV_Prevalence$Country)
+                  )
+                )
+              ),
+              
+              mainPanel(
+                plotOutput("Country_density")
+              )
+            ), 
       
-      ), 
-      selectizeInput(
-        inputId  = "prevalence.include.countries",
-        label    = "Include Countries",
-        choices  = unique(HPV_Prevalence$Country), 
-        multiple = FALSE,
-        selected = unique(HPV_Prevalence$Country)
+      tabItem(tabName = "Related_cancers", 
+              sidebarPanel(
+                selectInput(
+                  inputId = "country",
+                  label = "Country",
+                  choices = list(unique(HPV_cancers$country),
+                                 
+                  ),
+                  multiple = TRUE,
+                  selected = unique(HPV_cancers$country)
+                ),
+                selectizeInput(
+                  
+                  
+                  inputId = "age_range",
+                  label = "Age Range",
+                  choices = unique(HPV_cancers$age_range),
+                  multiple = TRUE,
+                  selected = unique(HPV_cancers$age_range)
+                ), 
+                
+                mainPanel(
+                  plotOutput("HPV_relcancers_prevalencebyage")
+                )
+              )
+            )
+          )
+        )
       )
-    )
-  ),
-  
-  mainPanel(
-    plotOutput("country.density")
-  )
-)
-
-fluidPage(
-  
-  title = "HPV Related Cancers and Prevalence in Different Countries",
-  titlePanel("HPV Realted Cancers"),
-  sidebarLayout(
-    sidebarPanel(
-      selectInput(
-        inputId = "country",
-        label = "Country",
-        choices = list(unique(HPV_cancers$country),
-                       
-        ),
-        multiple = TRUE,
-        selected = unique(HPV_cancers$country)
-      ),
-      selectizeInput(
-        
-        
-        inputId = "age_range",
-        label = "Age Range",
-        choices = unique(HPV_cancers$age_range),
-        multiple = TRUE,
-        selected = unique(HPV_cancers$age_range)
-      )
-    )
-  ),
-    
-    mainPanel(
-      plotOutput("HPV_relcancers_prevalencebyage")
-    )
-)
