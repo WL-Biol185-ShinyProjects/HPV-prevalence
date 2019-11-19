1c532d5a70eb6d5fcaf7dbf8c6839714650ede76
Mikki Whittington <whittingtono20@mail.wlu.edu>
2019-11-19 13:02:59 -0500

    Working on ggplot Prevalence Bar graph

--- server.r
+++ server.r
@@ -5,10 +5,10 @@ library(tidyverse)
 function(input, output) {
   
   output$Country_bar <- renderPlot({
-    HPV_Prevalence %>%
-      filter(Country %in% input$Prevalence_include_countries) %>%
-      ggplot(aes_string("HPV_Prevalence", fill = input$Prevalence_group_by)) +
-      geom_density(alpha = 0.2)
+    HPV_Prevalence      %>%
+      filter(Country    %in% input$Prevalence_include_countries)    %>%
+      ggplot(aes_string(Country, Prevalence)) +
+      geom_bar(stat = 'identity')
   })
 
   output$Cancer_bar <- renderPlot({
