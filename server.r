9817d6fd61fd6338f7c80c598e33423bed492688
Whitley Drinkard <drinkardw20@mail.wlu.edu>
2019-12-03 12:56:37 -0500

    Formatting

--- server.r
+++ server.r
@@ -25,7 +25,7 @@ function(input, output) {
   
   output$Cervical_cancer_bar <- renderPlot({
     
-    HPV_cervicalcancers                         %>%
+    HPV_cervicalcancers                             %>%
       filter(country %in% input$country_choice_cer) %>%
       filter(age_range %in% input$ages_cer)         %>%
       ggplot(aes_string("country", "number_of_cases", fill = "age_range")) + 
