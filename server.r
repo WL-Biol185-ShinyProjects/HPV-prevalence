fabe67fac5eee74e3827db0c19d1605079350ac5
Mikki Whittington <whittingtono20@mail.wlu.edu>
2019-12-03 12:42:38 -0500

    Still fixing merge conflicts

--- server.r
+++ server.r
@@ -6,10 +6,9 @@ function(input, output) {
   
   output$Country_bar <- renderPlot({
     HPV_Prevalence      %>%
-      arrange_(input$y_axis) %>%
       filter(Country    %in% input$Prevalence_include_countries)    %>%
-      ggplot(aes_string(Country, Prevalence)) +
-      geom_bar(stat = 'identity')
+      ggplot(aes_string("HPV_Prevalence", fill = input$Prevalence_group_by)) +
+      geom_density(alpha = 0.2)
   })
 
   output$Cancer_bar <- renderPlot({
